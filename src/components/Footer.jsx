c3e1b7bd18a852f09e23008543210dc8fa2de7d5
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-04-06 16:09:21 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const quickLinks = [
 ];
 
 const supportLinks = [
-  { label: "Contact", href: "mailto:hello@mapcanvas.co" },
-  { label: "Help Center", href: "mailto:support@mapcanvas.co" },
+  { label: "Contact", href: "mailto:jeremyrellama17@gmail.com" },
+  { label: "Help Center", href: "mailto:jeremyrellama17@gmail.com" },
   { label: "Privacy", href: "#" },
   { label: "Terms", href: "#" },
 ];
@@ -29,7 +29,7 @@ const socialLinks = [
   },
   {
     label: "Facebook",
-    href: "#",
+    href: "https://www.facebook.com/JEREMY.RELLAMA17/",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" className="size-5" aria-hidden="true">
         <path d="M13.46 21v-7.25h2.43l.36-2.82h-2.8V9.13c0-.82.23-1.37 1.41-1.37h1.5V5.23c-.26-.04-1.16-.11-2.2-.11-2.18 0-3.67 1.33-3.67 3.76v2.05H8v2.82h2.49V21h2.97Z" />
@@ -37,12 +37,10 @@ const socialLinks = [
     ),
   },
   {
-    label: "Pinterest",
-    href: "#",
+    label: "Github",
+    href: "https://github.com/PaperNsalt",
     icon: (
-      <svg viewBox="0 0 24 24" fill="currentColor" className="size-5" aria-hidden="true">
-        <path d="M12 3.2c-4.78 0-7.2 3.42-7.2 6.27 0 1.73.66 3.28 2.08 3.86.23.1.44 0 .5-.24.05-.18.16-.64.21-.83.07-.24.04-.32-.15-.56-.42-.5-.68-1.14-.68-2.05 0-2.64 1.98-5 5.15-5 2.8 0 4.33 1.71 4.33 4 0 3.01-1.33 5.55-3.3 5.55-1.09 0-1.9-.9-1.64-2 .31-1.3.9-2.7.9-3.64 0-.84-.45-1.54-1.38-1.54-1.09 0-1.96 1.13-1.96 2.64 0 .96.32 1.6.32 1.6l-1.3 5.48c-.38 1.61-.06 3.58-.03 3.78.02.12.16.15.22.06.1-.13 1.34-1.66 1.76-3.2.12-.43.67-2.62.67-2.62.33.63 1.3 1.19 2.33 1.19 3.07 0 5.15-2.8 5.15-6.55 0-2.84-2.41-5.48-6.08-5.48Z" />
-      </svg>
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
     ),
   },
 ];
